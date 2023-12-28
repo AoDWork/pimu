22d88b625655a9f7a9a2345566c213048db0f889
AoD <aodtest07@gmail.com>
2023-12-28 15:11:57 +0200

    circular progress bar in work

--- js/main.js
+++ js/main.js
@@ -1,39 +1,13 @@
 $(document).ready(function () {
     $('.slider').slick({
+        autoplay: true,
+        autoplaySpeed: 5000,
         dots: true,
         draggable: false,
         waitForAnimate: false,
         centerMode: false,
         variableWidth: false,
         fade: true,
-        appendDots:$('.slider__controls'),
+        appendDots:$('.slider__dots'),
     });
-    // $('.slider-track').slick({
-    //     arrows: false,
-    //     slidesToShow:0,
-
-    // });
 });
-
-function setProgress(index) {
-    const calc = ((index + 1) / ($slider.slick('getSlick').slideCount)) * 100;
-    const currentSlide = $slider.slick('getSlick').currentSlide
-  
-    $progressBar
-      .css('background-size', `${calc}% 100%`)
-      .attr('aria-valuenow', calc);
-  
-    $progressBarLabel.text(currentSlide);
-  }
-  
-  const $slider = $('.slider');
-  const $progressBar = $('.progress');
-  const $progressBarLabel = $('.slider__label');
-  
-  $slider.on('beforeChange', function(event, slick, currentSlide, nextSlide) {   
-    setProgress(nextSlide);
-  });
-  
-  
-  setProgress(0);
-
